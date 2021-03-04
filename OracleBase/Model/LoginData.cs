a9bf0f5fdd460479937727a01d7b61371542bdac
Seweryn <pikulikseweryn@gmail.com>
2021-03-04 19:44:29 +0100

    LoginData clean

--- OracleBase/Model/LoginData.cs
+++ OracleBase/Model/LoginData.cs
@@ -13,10 +13,5 @@ namespace OracleBase.Model
         public string Login = "DEV_DATA_1";
         public string Password = "";
         public string Message = "Enter your login details and click Test connection.";
-
-        public void TryLogin()
-        {
-            Message = "Try to login...";
-        }
     }
 }
